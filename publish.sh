fa276153e4e464d2e383cdca48d299dc3cd1b420
Kim Shepherd <kim@shepherd.nz>
2024-04-04 11:46:38 +1300

    Explicit PyPI repository reference in publish script

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 python setup.py bdist_wheel
-twine upload dist/dspace_rest_client-0.1.10-py3-none-any.whl
+twine upload --repository dspace-rest-client dist/dspace_rest_client-0.1.10-py3-none-any.whl
