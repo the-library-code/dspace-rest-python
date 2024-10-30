f60832933cbf17471bc5c797e211b2202e3699a6
Kim Shepherd <kim@shepherd.nz>
2024-10-30 16:04:00 +0100

    maintain version number in __init__.py

--- publish.sh
+++ publish.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
-python setup.py bdist_wheel
-twine upload --repository dspace-rest-client dist/dspace_rest_client-0.1.12-py3-none-any.whl
+
+version=$1
+
+if ! [[ $version =~ ^[0-9]+\.+[0-9]+\.[0-9]+$ ]]; then
+  echo "Usage: publish.sh <version>"
+  echo "Version must match the form x.y.z where all parts are numbers e.g. 0.1.13"
+  exit 1
+fi
+
+echo "Building dspace_rest_client version ${version}"
+if ! python setup.py bdist_wheel; then
+  echo "Error: Failed to build the package"
+  exit 1
+fi
+
+echo "Uploading dspace_rest_client version ${version}"
+if ! twine upload --repository dspace-rest-client dist/dspace_rest_client-0.1.13-py3-none-any.whl; then
+  echo "Error: Failed to upload to PyPI"
+  exit 1
+fi
+
+echo "...done"
+exit 0
